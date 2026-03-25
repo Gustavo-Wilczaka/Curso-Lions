0cd28d8ae1dd645457de80340e2b53e032505c6c
Gustavo Wilczak <gu.wilczak@gmail.com>
2026-03-25 14:31:24 -0300

    ex04 da lista de vetores

--- lista-vetor-01/ex02.js
+++ lista-vetor-01/ex02.js
@@ -1,5 +1,5 @@
-let nums=[2,8,4,9,10]; 
- 
-for(let i=0; i<nums.length; i++){
-    console.log("indice" , i , "=" , nums[i]);
+let nums = [2, 8, 4, 9, 10];
+
+for (let i = 0; i < nums.length; i++) {
+    console.log("indice", i, "=", nums[i]);
 }
---
+++ lista-vetor-01/ex04.js
@@ -0,0 +1,10 @@
+let nums = [2, 4, 6, 8, 10];
+
+let media = 0;
+
+for (let i = 0; i < nums.length; i++) {
+    media = media + nums[i]
+
+}
+media= media/nums.length;
+console.log(media)
